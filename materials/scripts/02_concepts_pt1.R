1158e18784172225ac3773097bfbf7be96b19852
Marco Sciaini <marco@kaldera.dev>
2022-07-24 09:58:04 -0400

    update

--- materials/scripts/02_concepts_pt1.R
+++ materials/scripts/02_concepts_pt1.R
@@ -29,7 +29,7 @@ library(tidyverse)
 
 ## -----------------------------------------------------------------------------
 bikes <- readr::read_csv(
-  "https://raw.githubusercontent.com/z3tt/graphic-design-ggplot2/main/data/london-bikes-custom.csv", 
+  here::here("data", "london-bikes-custom.csv"), 
   col_types = "Dcfffilllddddc"
 )
 
